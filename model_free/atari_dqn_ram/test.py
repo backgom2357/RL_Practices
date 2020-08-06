e0c8c31ef58f943f909b1f89a0766558d0bae0f4
Diominor <backgom2357@gmail.com>
2020-08-06 18:16:46 +0900

    add cam

--- model_free/atari_dqn_ram/test.py
+++ model_free/atari_dqn_ram/test.py
@@ -16,13 +16,15 @@ tf.config.experimental.set_memory_growth(
 
 cf = Config()
 
-def test(env_name, weights_url, render=False, check_input_frames=False, check_log_plot=False):
+def test(env_name, weights_url, render=False, check_input_frames=False, check_log_plot=False, check_saliency_map=False):
 
     # matplotlib 설정
     is_ipython = 'inline' in matplotlib.get_backend()
     if is_ipython:
         from IPython import display
     plt.ion()
+
+    cm = plt.get_cmap('jet')
 
     # environment
     env_name = env_name
@@ -31,6 +33,7 @@ def test(env_name, weights_url, render=False, check_input_frames=False, check_lo
     action_dim = env.action_space.n
     
     model = build_duel_dqn_model(cf.frame_size, action_dim, cf.agent_history_length)
+    model.summary()
     model.load_weights(weights_url)
     # print(model.trainable_weights)
 
@@ -77,7 +80,21 @@ def test(env_name, weights_url, render=False, check_input_frames=False, check_lo
         if check_input_frames:
             test_img = np.reshape(next_seq, (84, 84, 4))
             test_img = cv2.resize(test_img, dsize=(300, 300), interpolation=cv2.INTER_AREA)
-            cv2.imshow('obs', test_img)
+            cv2.imshow('input image', test_img)
+            cv2.waitKey(0)!=ord('l')
+            if cv2.waitKey(25)==ord('q') or done:
+                cv2.destroyAllWindows()
+        
+        # # check what the agent see
+        if check_saliency_map:
+            grad_img = generate_grad_cam(model, seq, reward, action, next_seq, done, 'conv2d', output_layer='dense_3')
+            grad_img = np.reshape(grad_img, (84, 84))
+            grad_img = cm(grad_img)[:,:,:3]
+            screen = env.render(mode='rgb_array')
+            screen, grad_img = cv2.resize(screen, dsize=(400,500))/255., cv2.resize(grad_img, dsize=(400,500))
+            test_img = cv2.addWeighted(screen,0.5,grad_img,0.5,0,dtype=cv2.CV_32F)
+            test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+            cv2.imshow('saliency map', test_img)
             cv2.waitKey(0)!=ord('l')
             if cv2.waitKey(25)==ord('q') or done:
                 cv2.destroyAllWindows()
@@ -91,6 +108,27 @@ def test(env_name, weights_url, render=False, check_input_frames=False, check_lo
 
     plt.ioff()
 
+def generate_grad_cam(model, state, reward, action, next_state, done, activation_layer, output_layer):
+    
+    height, width = cf.frame_size, cf.frame_size
+    grad_cam_model = tf.keras.models.Model(model.inputs, [model.get_layer(activation_layer).output, model.get_layer(output_layer).output])
+
+    with tf.GradientTape() as g:
+        # argmax action from current q
+        layer_output, pred = grad_cam_model(normalize(state))
+        grad = g.gradient(pred, layer_output)[0]
+    weights = np.mean(grad, axis=(0,1))
+    # print(weights.shape)
+
+    grad_cam_image = np.zeros(dtype=np.float32, shape=layer_output.shape[0:2])
+    for i, w in enumerate(weights):
+        # W * f 를 통해 class별 activation map을 계산합니다.
+        grad_cam_image = w * layer_output[0, :, :, i]
+
+    grad_cam_image /= np.max(grad_cam_image) # activation score를 normalize합니다.
+    grad_cam_image = grad_cam_image.numpy()
+    grad_cam_image = cv2.resize(grad_cam_image, (width, height)) # 원래 이미지의 크기로 resize합니다.
+    return grad_cam_image
 
 def plot_durations(q, is_ipython):
     ACTION_MEANING = {
@@ -134,5 +172,5 @@ def plot_durations(q, is_ipython):
         display.display(plt.gcf())
 
 if __name__ == "__main__":
-    env_setting = [cf.ATARI_GAMES[1], './save_weights/dqn_boxing_3600epi.h5']
-    test(*env_setting, render=True, check_log_plot=True)
+    env_setting = [cf.ATARI_GAMES[0], './save_weights/dqn_breakout_6600epi.h5']
+    test(*env_setting, check_log_plot=True, check_saliency_map=True)
